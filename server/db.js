ee9ae65adb4a6f1b16992c057366808038fac5a6
Dennis Hsu <denniseh7@gmail.com>
2023-04-06 15:02:04 -0700

    simple testing and file paths

--- server/db.js
+++ server/db.js
@@ -1,12 +1,14 @@
 const postgres = require('postgres');
+require('dotenv').config({ path: '../.env' });
 // trying postgres.js instead of node-postgres(pg) first
+console.log('dotenv', process.env.PORT);
 
-const sql = postgres('postgres://username:password@http:/localhost:3001/database', {
-  host: 'localhost',
-  port: '5432',
-  database: 'koiapi',
-  username: 'me',
-  password: 'password',
+const sql = postgres('postgres://username:password@http:/host:port/database', {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
 });
 
 async function getReviews() {
@@ -16,7 +18,7 @@ async function getReviews() {
       id,
       reviewer_name
     from reviews
-    limit 10
+    limit 5
     `;
     return response;
   } catch (err) {
